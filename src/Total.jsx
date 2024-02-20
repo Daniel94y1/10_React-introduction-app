a36d913d2f071cd52c2c7e45538b8d9f256da253
Daniel94y1 <escobardaniel08@gmail.com>
2024-02-20 18:07:37 +0100

    Install eslint and error lens extension

--- src/Total.jsx
+++ src/Total.jsx
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+
 
 export const Total = ({ exercises1, exercises2, exercises3 }) => {
   return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
